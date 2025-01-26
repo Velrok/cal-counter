92116149efbb63fa56dfe41692da67d31046ba1d
localpigeon63 <152446902+localpigeon63@users.noreply.github.com>
2025-01-26 16:08:41 +0000

    State file created

--- src/main/cal_counter/frontend/app.cljs
+++ src/main/cal_counter/frontend/app.cljs
@@ -2,48 +2,35 @@
   (:require [reagent.core :as r]
             ["react" :as react]
             ["react-dom/client" :as react-dom]
-            [cal-counter.frontend.ui :as ui]))
+            [cal-counter.frontend.ui :as ui]
+            [cal-counter.frontend.state :as state]))
 
 ; (def male-daily-min 1700)
 ; (def male-daily-maintain 2200)
-
-(defn cal-progress [{:keys [cals label]}]
-  (let [id  (keyword label)]
-    [:div
-     [:label {:for id} (str label "(" cals ")")]]))
-
-(defn cal-progress-day [{:keys [cals label]}]
-  (cal-progress {:cals cals :label label}))
-
-(def min-calories
-  (r/atom (or (.getItem js/localStorage "min-calories") 2000)))
-
-(def max-calories
-  (r/atom (or (.getItem js/localStorage "max-calories") 2500)))
 
 (defn settings []
   [:div
    [:h2 "Settings"]
    [:p "Target calorie min"]
-   [:input {:value @min-calories
+   [:input {:value @state/min-calories
             :type "number"
             :on-change (fn [event]
                          (->> event
                               .-target
                               .-value
-                              (reset! min-calories)))}]
+                              (reset! state/min-calories)))}]
    [:p "Target calorie max"]
-   [:input {:value @max-calories
+   [:input {:value @state/max-calories
             :type "number"
             :on-change (fn [event]
                          (->> event
                               .-target
                               .-value
-                              (reset! max-calories)))}]
+                              (reset! state/max-calories)))}]
    [:button {:on-click
              (fn []
-               (.setItem js/localStorage "min-calories" @min-calories)
-               (.setItem js/localStorage "max-calories" @max-calories))}
+               (.setItem js/localStorage "min-calories" @state/min-calories)
+               (.setItem js/localStorage "max-calories" @state/max-calories))}
     "Save"]])
 
 (def main-stage
@@ -66,5 +53,6 @@
    [tabs]])
 
 (defn init []
+  (println "innit")
   (let [root (react-dom/createRoot (.getElementById js/document "root"))]
     (.render root (r/as-element [app]))))
---
+++ src/main/cal_counter/frontend/state.cljs
@@ -0,0 +1,8 @@
+(ns cal-counter.frontend.state
+(:require [reagent.core :as r]))
+
+(def min-calories
+  (r/atom (or (.getItem js/localStorage "min-calories") 2000)))
+
+(def max-calories
+  (r/atom (or (.getItem js/localStorage "max-calories") 2500)))

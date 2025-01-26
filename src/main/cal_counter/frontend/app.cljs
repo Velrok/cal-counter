482d587685b49358f67fb63084999d63f5feb575
Waldemar <34974+Velrok@users.noreply.github.com>
2025-01-26 17:45:23 +0000

    refactor UI; introduce ui/title; add label to input

--- src/main/cal_counter/frontend/app.cljs
+++ src/main/cal_counter/frontend/app.cljs
@@ -1,6 +1,6 @@
 (ns cal-counter.frontend.app
   (:require [reagent.core :as r]
-            ["react" :as react]
+            ; ["react" :as react]
             ["react-dom/client" :as react-dom]
             [cal-counter.frontend.ui :as ui]
             [cal-counter.frontend.state :as state]))
@@ -11,11 +11,14 @@
 (defn settings []
   [:div
    [:h2 "Settings"]
-   [:p "Target calorie min"]
-   [ui/input state/min-calories]
-              
-   [:p "Target calorie max"]
-   [ui/input state/max-calories]
+
+   [ui/input
+    {:label  "Target calorie min"
+     :atom state/min-calories}]
+
+   [ui/input
+    {:label  "Target calorie max"
+     :atom state/max-calories}]
    [:button {:on-click
              (fn []
                (state/save-min-calories)
@@ -37,11 +40,10 @@
                  :grid-template-columns "100%"
                  :grid-template-rows "4em 1fr 60px"
                  :height "97vh"}}
-   [:h1 "CalCounter"]
+   [ui/app-title]
    [@main-stage]
    [tabs]])
 
 (defn init []
-  (println "innit")
   (let [root (react-dom/createRoot (.getElementById js/document "root"))]
     (.render root (r/as-element [app]))))
--- src/main/cal_counter/frontend/ui.cljs
+++ src/main/cal_counter/frontend/ui.cljs
@@ -3,11 +3,18 @@
 (defn reload []
   [:button {:on-click #(.reload js/location)} "reload"])
 
-(defn input [atom]
-  [:input {:value @atom
-           :type "number"
-           :on-change (fn [event]
-                        (->> event
-                             .-target
-                             .-value
-                             (reset! atom)))}])
+(defn input [{:keys [atom label]}]
+  [:div
+   [:p label]
+   [:input {:value @atom
+            :type "number"
+            :on-change (fn [event]
+                         (->> event
+                              .-target
+                              .-value
+                              (reset! atom)))}]])
+
+(defn app-title []
+  [:h1#title.title
+   {:style {:textAlign "center"}}
+   "CalCounter"])
